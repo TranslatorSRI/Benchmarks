e4b2a18f5c2dcdcfe59370916c39aaeaf0150166
Chris Bizon <bizon@email.unc.edu>
2022-02-07 17:31:39 -0500

    Simplified scoring/definition; added runner config

--- BenchmarkRunner/runner.py
+++ BenchmarkRunner/runner.py
@@ -3,66 +3,84 @@ from copy import deepcopy
 import json
 import os
 from csv import DictReader
+from collections import defaultdict
+import argparse
 
 class SystemRunner:
-    def __init__(self,url):
-        self.url= url
+    def __init__(self,configfile):
+        with open(configfile,'r') as inf:
+            config = json.load(inf)
+        self.url= config['url']
+        if 'workflow' in config:
+            self.workflow = config['workflow']
+        else:
+            self.workflow = None
+        self.result_cache = {}
     def syncquery(self,message):
+        #We are going to cache results to this endpoint so we don't ask the same question over and over
+        # when we have multiple results we are looking for.
+        #This is memory inefficient.  It would probably be smarter to group the queries ahead of time
+        smess = json.dumps(message,sort_keys=True,indent = 4)
+        if smess in self.result_cache:
+            return self.result_cache[smess]
         qurl = f'{self.url}/query'
         results = requests.post(qurl,json=message)
-        print(results.status_code)
-        return results.json()
-    def run(self,message):
-        return self.syncquery(message)
-
-class AragornRunner(SystemRunner):
-    def __init(self,url):
-        super(url)
+        j = results.json()
+        self.result_cache[smess] = j
+        return j
     def run(self,message):
         newmessage = deepcopy(message)
-        newmessage['workflow'] = [{'id':'lookup'},{'id':'overlay_connect_knodes'},{'id':'score'}]
+        if self.workflow is not None:
+            newmessage['workflow'] = self.workflow
         return self.syncquery(newmessage)
+    def dump_cache(self):
+        i = 0
+        for k,v in self.result_cache.items():
+            with open(f'result_{i}.json','w') as outf:
+                json.dump(v,outf,indent=4)
+            i += 1
 
 class Accumulator:
     def __init__(self,name):
         self.test_name = name
-        self.ntests = 0
-        self.npass = 0
-    def accumulate(self,result):
-        self.ntests += 1
-        if result:
-            print('Pass')
-            self.npass  += 1
+        self.ranks = []
+    def accumulate(self,result_found,result_rank):
+        if not result_found:
+            self.ranks.append(False)
         else:
-            print('Fail')
+            self.ranks.append(result_rank)
     def generate_report(self):
-        print(f'Ran {self.ntests} tests.')
-        print(f'Passed {self.npass} tests.')
+        for rank in self.ranks:
+            print(rank)
 
-class TopNScorer:
+
+class Scorer:
     def __init__(self,bdef,data_map,case):
         #Where do we get the rank we must achieve?
-        col_name = bdef["N_source_column"]
-        #And what does that rank need to be?
-        self.rank = int(case[col_name])
+        self.rank = int(bdef['N'])
         #What node in the result graph do we need to look at and what should we find?
         output_nodes = data_map['node_mappings']['output']
         self.expected_node_mappings = { node_id: case[case_column] for node_id, case_column in output_nodes.items() }
     def score(self,response):
         results = response['message']['results']
-        #Only look in the top N results for the hits
-        success = { node_id: False for node_id in self.expected_node_mappings }
-        for result in results[:min(self.rank,len(results))]:
-            for node_id, expected_value in self.expected_node_mappings.items():
-                print('Expected', expected_value)
-                output_ids = [x['id'] for x in result['node_bindings'][node_id]]
-                print('Found', output_ids)
-                if expected_value in output_ids:
-                    success[node_id] = True
-        all_success = success.values()
-        if False in all_success:
-            return False
-        return True
+        #First pull the relevant parts of all results.  The important part might show up in more than one result
+        results_to_rank = defaultdict(list)
+        for rank,result in enumerate(results):
+            #if there are multiple knodes per qnode, this will be wrong
+            found_node_mappings = {node_id: [x['id'] for x in result['node_bindings'][node_id]][0] for node_id in self.expected_node_mappings}
+            js = json.dumps(found_node_mappings,sort_keys=True)
+            results_to_rank[js].append(rank+1)
+        #for j,n in results_to_rank.items():
+        #    print(f'Result: {j}')
+        #    print(n)
+        e = json.dumps(self.expected_node_mappings,sort_keys=True)
+        print('looking for',e)
+        if e not in results_to_rank:
+            print('Not found')
+            return False,0
+        rank = min(results_to_rank[e])
+        print(f'Found at rank {rank}')
+        return True,rank
 
 def template_message(query_template, data_map, case):
     q = deepcopy(query_template)
@@ -78,11 +96,18 @@ def generate_tests(benchmark_definition,benchmark_dir):
     with open(os.path.join(benchmark_dir,benchmark_definition['mapping_file']),'r') as inf:
         data_map = json.load(inf)
     datadir = os.path.join(benchmark_dir.split('/')[0],'Data')
+    mx = 3
+    n=0
     with open(os.path.join(datadir,benchmark_definition['case_file']),'r') as inf:
         reader = DictReader(inf,delimiter='\t')
         for case in reader:
-            message = template_message(query_template,data_map,case)
-            yield message,TopNScorer(benchmark_definition,data_map,case)
+            if n < mx:
+                message = template_message(query_template,data_map,case)
+                n += 1
+                yield message,Scorer(benchmark_definition,data_map,case)
+            else:
+                break
+    print('all done')
 
 def run_benchmark(benchmark_definition_file,runner):
     benchmark_dir = os.path.dirname(benchmark_definition_file)
@@ -92,9 +117,29 @@ def run_benchmark(benchmark_definition_file,runner):
     test_generator = generate_tests(bdef,benchmark_dir)
     for test_message, scorer  in test_generator:
         result = runner.run(test_message)
-        result_score = scorer.score(result)
-        result_accumulator.accumulate(result_score)
+        found, result_score = scorer.score(result)
+        result_accumulator.accumulate(found, result_score)
+    runner.dump_cache()
     result_accumulator.generate_report()
 
+def handle_args():
+    parser = argparse.ArgumentParser(description='Run queries to produce unscored results')
+    parser.add_argument('--defs', type=str, nargs='+', help='Paths to benchmark_definition')
+    parser.add_argument('--runners', type=str, nargs='+', help='Paths to runner configurations')
+    args = parser.parse_args()
+    return args
+
+def run(args):
+    defs = args.defs
+    runconfigs = args.runners
+    runners = [ SystemRunner(runconfig) for runconfig in runconfigs ]
+    for bench_def in defs:
+        #er, right now, just use the first runner...
+        run_benchmark(bench_def,runners[0])
+
 if __name__ == '__main__':
-    run_benchmark('MostPrescribed_2019/benchmarks/treats/benchmark_definition.json',AragornRunner('https://aragorn.renci.org/1.2'))
+    #python BenchmarkRunner/runner.py --defs MostPrescribed_2019/benchmarks/treats/benchmark_definition.json
+    run(handle_args())
+
+#if __name__ == '__main__':
+#    run_benchmark('MostPrescribed_2019/benchmarks/treats/benchmark_definition.json',AragornRunner('http://aragorn-dev.apps.renci.org/1.2'))
