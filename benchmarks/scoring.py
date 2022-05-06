3fabb92443e2fade2c37cbf21e788bdcfdfbd5f0
Adam Viola <aviola@covar.com>
2022-05-06 09:57:37 -0400

    Added docs for scoring.py

--- benchmarks/scoring.py
+++ benchmarks/scoring.py
@@ -11,6 +11,20 @@ from typing import Sequence, Tuple, Union
 from benchmarks.utils.benchmark import benchmark_ground_truth
 
 def evaluate_results(benchmark: str, results_dir: str, k: int = 50) -> 'BenchmarkResults':
+    """
+    Computes metrics on the query results associated with the specified
+    benchmark. See BenchmarkResults for the complete list of metrics.
+
+    Args:
+        benchmark: (str) Name of the benchmark to run; see benchmarks.json for a
+            complete list of benchmarks.
+        reuslts_dir: (str) Path to the directory containing scored results to
+            to the queries associated with the specified benchmark.
+        k: (int) Maximum number of results to consider for scoring.
+
+    Returns:
+        BenchmarkResults object with functions for getting and plotting metrics.
+    """
     uids, gts = benchmark_ground_truth(benchmark)
     results_dir = Path(results_dir)
 
@@ -74,6 +88,49 @@ def evaluate_results(benchmark: str, results_dir: str, k: int = 50) -> 'Benchmar
 
 
 class BenchmarkResults:
+    """
+    Object that contains functions for getting and plotting metrics.
+    
+    Metrics:
+
+    Precision @ k
+        Out of the top-k results, what fraction of them are relevant?
+            Single query:
+                `Number of relevant results (in top-k) / k`
+            Multiple queries: 
+                `Number of relevant results (in top-k) / (Number of queries * k)`
+    
+    Recall @ k 
+        Out of all relevant results, what fraction of them are in the top-k?
+            Single query:
+                `Number of relevant results (in top-k) / Number of relevant results`
+            Multiple queries: 
+                `Total number of relevant results (in top-k) / Total number of relevant results`
+
+    Mean Average Precision (mAP) @ k
+        Average Precision @ k, averaged over all queries. Intuitively, mAP @ k
+        is similar to Precision @ k, but it factors in the ranking of the
+        relevant results (better ranking of relevant results -> higher mAP).
+
+    Top-k Accuracy
+        The fraction of instances where the correct result is among the top-k
+        results.
+        
+        Note that this metric requires a "correct" result (only 1
+        relevant result), so it treats each row in the data (aka each relevant
+        result) as a separate data case. As a result, this metric will count
+        queries that have `n` relevant results `n` times. This metric is useful
+        when the queries each have a small number of relevant results.
+
+        Note that Top-k Accuracy is equal to Recall @ k under this definition.
+
+    Mean Reciprocal Rank
+        Reciprocal of the rank of the first relevant result, averaged over all
+        queries.
+
+        Equivalently, MRR is the reciprocal of the harmonic mean (across all
+        queries) of the rank of the first relevant result.
+    """
     def __init__(self, tp_k, map_k, mrr, num_relevant, num_queries):
         self.k = np.arange(1, len(tp_k) + 1)
         self.p_k = tp_k / (self.k * num_queries)
@@ -88,15 +145,51 @@ class BenchmarkResults:
         return getattr(self, metric)[k - 1]
 
     def precision_at_k(self, k: Union[int, Sequence[int]]) -> float:
+        """
+        Computes the Precision @ k.
+        
+        Args:
+            k: (int) Number of results to consider for Precision @ k.
+
+        Returns:
+            Precision @ k (float)
+        """
         return self._metric_at_k('p_k', k)
 
     def recall_at_k(self, k: Union[int, Sequence[int]]) -> float:
+        """
+        Computes the Recall @ k.
+        
+        Args:
+            k: (int) Number of results to consider for Recall @ k.
+
+        Returns:
+            Recall @ k (float)
+        """
         return self._metric_at_k('r_k', k)
 
     def mAP_at_k(self, k: Union[int, Sequence[int]]) -> float:
+        """
+        Computes the mAP @ k
+        
+        Args:
+            k: (int) Number of results to consider for mAP @ k.
+
+        Returns:
+            mAP @ k (float)
+        """
         return self._metric_at_k('map_k', k)
 
     def top_k_accuracy(self, k: Union[int, Sequence[int]]) -> float:
+        """
+        Computes the Top-k Accuracy.
+        
+        Args:
+            k: (int) Number of results to consider for Top-k Accuracy.
+
+        Returns:
+            Top-k Accuracy (float)
+        """
         return self._metric_at_k('top_k_acc', k)
 
     def _plot_metric(self, x_field: str, y_field: str, ax: Axes, label: str = None, xlabel: str = None, ylabel: str = None, xlim: Tuple[int] = None, ylim: Tuple[int] = None) -> Axes:
