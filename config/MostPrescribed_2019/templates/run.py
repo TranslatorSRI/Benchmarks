085fe2d32ebba66ccb69c95e4c39d2f179862009
erinma6 <erinma6@gmail.com>
2022-11-15 23:02:48 -0500

    trying to run

---
+++ config/MostPrescribed_2019/templates/run.py
@@ -0,0 +1,8 @@
+from benchmarks.request import fetch_results, score_results
+from benchmarks.eval import evaluate_results
+import os
+
+##print(os.getcwd())
+fetch_results('ameliorates', 'aragorn', 'unscored_results_dir', scored=False)
+##score_results('unscored_results_dir', 'aragorn', 'results_dir')
+
