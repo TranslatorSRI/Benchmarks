7e114ac19474abdbfc5c8d02ace1c54a9c81353c
erinma6 <erinma6@gmail.com>
2022-11-16 22:13:40 -0500

    seeing if i can see better on git

--- config/MostPrescribed_2019/templates/run.py
+++ config/MostPrescribed_2019/templates/run.py
@@ -4,5 +4,6 @@ import os
 
 ##print(os.getcwd())
 fetch_results('ameliorates', 'aragorn', 'unscored_results_dir', scored=False)
-##score_results('unscored_results_dir', 'aragorn', 'results_dir')
+score_results('unscored_results_dir', 'aragorn', 'results_dir')
+evaluate_results('ameliorates', 'results_dir')
 
