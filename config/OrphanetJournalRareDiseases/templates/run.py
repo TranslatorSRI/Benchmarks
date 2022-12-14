e41fdb8f2b017df6b9312beecae80338290930bd
erinma6 <erinma6@gmail.com>
2022-12-13 23:12:05 -0500

    finished this branch

--- config/OrphanetJournalRareDiseases/templates/run.py
+++
@@ -1,9 +0,0 @@
-from benchmarks.request import fetch_results, score_results
-from benchmarks.eval import evaluate_results
-import os
-
-##print(os.getcwd())
-fetch_results('treats_rare', 'aragorn', 'unscored_results_dir', scored=False)
-score_results('unscored_results_dir', 'aragorn', 'results_dir')
-evaluate_results('treats_rare', 'evaluated')
-
