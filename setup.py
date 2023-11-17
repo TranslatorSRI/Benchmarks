5df69441ce12ddf35f161843ad67b5efd964fc07
Max Wang <max@covar.com>
2023-11-17 13:30:37 -0500

    Bump patch version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as readme_file:
 
 setup(
     name='benchmarks-runner',
-    version='0.1.0',
+    version='0.1.1',
     author="Max Wang",
     author_email="max@covar.com",
     url="https://github.com/TranslatorSRI/Benchmarks",
