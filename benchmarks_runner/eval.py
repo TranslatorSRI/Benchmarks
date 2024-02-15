7fc0032194ea1d67dc1cbdade08b837db249171c
Max Wang <max@covar.com>
2024-02-15 13:08:29 -0500

    Handle cases when messages have no results or analyses

--- benchmarks_runner/eval.py
+++ benchmarks_runner/eval.py
@@ -90,8 +90,8 @@ def evaluate_results(
                 message = results_dir.get(uid, {}).get("message", {})
 
         results_k = sorted(
-            message.get('results', []),
-            key=lambda r: r['analyses'][0].get('score', 0),
+            message.get('results') or [],
+            key=lambda r: r.get('analyses', [{ 'score': 0 }])[0].get('score', 0),
             reverse=True
         )[:k]
 
